77b172812e25ab428f53428713a9d310fd106a29
dangfan <dangf09@gmail.com>
2020-01-20 22:06:33 +0800

    add pboc_des_mac

--- assets/crypto.js
+++ assets/crypto.js
@@ -1,36 +1,71 @@
-function hexStringToArrayBuffer(hexString) {
-    var pairs = hexString.match(/[\dA-F]{2}/gi);
-    var integers = pairs.map(function(s) {
-        return parseInt(s, 16);
-    });
-    var array = new Uint8Array(integers);
-    return array.buffer;
+function _hexStringToArrayBuffer(hexString) {
+    const pairs = hexString.match(/[\dA-F]{2}/gi);
+    const integers = pairs.map(s => parseInt(s, 16));
+    return new Uint8Array(integers).buffer;
+}
+
+function _tdes2to3(key) {
+    const fullKey = new Uint8Array(24);
+    fullKey.set(new Uint8Array(key), 0);
+    fullKey.set(new Uint8Array(key).slice(0, 8), 16);
+    return CryptoJS.lib.WordArray.create(fullKey);
+}
+
+function _xor(a, b) {
+    const result = new Uint8Array(8);
+    for (let i = 0; i < 8; ++i)
+        result[i] = a[i] ^ b[i];
+    return result;
 }
 
 function des_encrypt(data, key) {
+    if (data.byteLength % 8 != 0) throw "invalid data length";
+    if (key.byteLength != 8) throw "invalid key length";
     data = CryptoJS.lib.WordArray.create(data);
     key = CryptoJS.lib.WordArray.create(key);
-    let result = CryptoJS.DES.encrypt(data, key, {mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.NoPadding});
-    return hexStringToArrayBuffer(result.ciphertext.toString())
+    const result = CryptoJS.DES.encrypt(data, key, {mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.NoPadding});
+    return _hexStringToArrayBuffer(result.ciphertext.toString())
 }
 
 function des_decrypt(data, key) {
+    if (data.byteLength % 8 != 0) throw "invalid data length";
+    if (key.byteLength != 8) throw "invalid key length";
     data = CryptoJS.lib.WordArray.create(data);
     key = CryptoJS.lib.WordArray.create(key);
-    let result = CryptoJS.DES.decrypt({ciphertext: data}, key, {mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.NoPadding});
-    return hexStringToArrayBuffer(result.toString())
+    const result = CryptoJS.DES.decrypt({ciphertext: data}, key, {mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.NoPadding});
+    return _hexStringToArrayBuffer(result.toString())
 }
 
 function tdes_encrypt(data, key) {
+    if (data.byteLength % 8 != 0) throw "invalid data length";
+    if (key.byteLength != 16) throw "invalid key length";
     data = CryptoJS.lib.WordArray.create(data);
-    key = CryptoJS.lib.WordArray.create(key);
-    let result = CryptoJS.TripleDES.encrypt(data, key, {mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.NoPadding});
-    return hexStringToArrayBuffer(result.ciphertext.toString())
+    const result = CryptoJS.TripleDES.encrypt(data, _tdes2to3(key), {mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.NoPadding});
+    return _hexStringToArrayBuffer(result.ciphertext.toString())
 }
 
 function tdes_decrypt(data, key) {
+    if (data.byteLength % 8 != 0) throw "invalid data length";
+    if (key.byteLength != 16) throw "invalid key length";
     data = CryptoJS.lib.WordArray.create(data);
-    key = CryptoJS.lib.WordArray.create(key);
-    let result = CryptoJS.TripleDES.decrypt({ciphertext: data}, key, {mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.NoPadding});
-    return hexStringToArrayBuffer(result.toString())
+    const result = CryptoJS.TripleDES.decrypt({ciphertext: data}, _tdes2to3(key), {mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.NoPadding});
+    return _hexStringToArrayBuffer(result.toString())
+}
+
+function pboc_des_mac(data, key, iv) {
+    if (key.byteLength != 8) throw "invalid key length";
+    if (iv.byteLength != 8) throw "invalid iv length";
+    const paddedSize = data.byteLength + (8 - data.byteLength % 8);
+    const paddedData = new Uint8Array(paddedSize);
+    paddedData.set(new Uint8Array(data));
+    paddedData[data.byteLength] = 0x80;
+    for (let i = data.byteLength + 1; i < paddedSize; ++i)
+        paddedData[i] = 0x00;
+    const result = new Uint8Array(8);
+    result.set(new Uint8Array(iv));
+    for (let i = 0; i < paddedSize / 8; ++i) {
+        result.set(_xor(result, paddedData.slice(i * 8, (i + 1) * 8)));
+        result.set(new Uint8Array(des_encrypt(result, key)));
+    }
+    return result.buffer;
 }
