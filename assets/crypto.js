8a84966bc0499b1a738b98c1e6c04858287e211b
dangfan <dangf09@gmail.com>
2020-01-20 18:55:55 +0800

    add tdes algorithm support

--- assets/crypto.js
+++ assets/crypto.js
@@ -10,6 +10,27 @@ function hexStringToArrayBuffer(hexString) {
 function des_encrypt(data, key) {
     data = CryptoJS.lib.WordArray.create(data);
     key = CryptoJS.lib.WordArray.create(key);
-    let result = CryptoJS.DES.encrypt(key, data, {mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.NoPadding});
+    let result = CryptoJS.DES.encrypt(data, key, {mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.NoPadding});
     return hexStringToArrayBuffer(result.ciphertext.toString())
+}
+
+function des_decrypt(data, key) {
+    data = CryptoJS.lib.WordArray.create(data);
+    key = CryptoJS.lib.WordArray.create(key);
+    let result = CryptoJS.DES.decrypt({ciphertext: data}, key, {mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.NoPadding});
+    return hexStringToArrayBuffer(result.toString())
+}
+
+function tdes_encrypt(data, key) {
+    data = CryptoJS.lib.WordArray.create(data);
+    key = CryptoJS.lib.WordArray.create(key);
+    let result = CryptoJS.TripleDES.encrypt(data, key, {mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.NoPadding});
+    return hexStringToArrayBuffer(result.ciphertext.toString())
+}
+
+function tdes_decrypt(data, key) {
+    data = CryptoJS.lib.WordArray.create(data);
+    key = CryptoJS.lib.WordArray.create(key);
+    let result = CryptoJS.TripleDES.decrypt({ciphertext: data}, key, {mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.NoPadding});
+    return hexStringToArrayBuffer(result.toString())
 }
