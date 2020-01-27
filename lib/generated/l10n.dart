511aedf5f3a39780d520102e18221d7714b21e08
dangfan <dangf09@gmail.com>
2020-01-27 18:12:54 +0800

    upgrade nfc_kit

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -62,7 +62,7 @@ class AppLocalizationDelegate extends LocalizationsDelegate<S> {
 
   List<Locale> get supportedLocales {
     return const <Locale>[
-      Locale('zh', ''), Locale('en', ''),
+      Locale('en', ''), Locale('zh', ''),
     ];
   }
 
