a9b2b84618c9a051019c7d501be6ec2a1f0f3a86
z4yx <z4yx@users.noreply.github.com>
2020-05-06 17:09:32 +0800

    fix the Felica reader

--- assets/felica.js
+++ assets/felica.js
@@ -3,12 +3,15 @@ class Felica {
         this.transceive = transceive;
     }
     async sendCommand(cmd, payload) {
+        if (cmd != this.CMD_POLLING) {
+            const idm_buf = new Uint8Array(hex2buf(this.idm));
+            payload = Array.from(idm_buf).concat(payload);
+        }
         let buf = Uint8Array.from([payload.length+2, cmd, ...payload]);
         let resp = await this.transceive(buf2hex(buf));
         if(!resp || resp.length < 20) {
             throw new Error("Invalid Felica response");
         }
-        // buf = hex2buf(resp);
         this.idm = resp.slice(4, 20);
         return resp;
     }
@@ -23,10 +26,10 @@ class Felica {
         let r = await this.sendCommand(this.CMD_READ_WO_ENCRYPTION, [
             1, serviceCode & 0xff, serviceCode >> 8, 1, 0x80, addr
         ]);
-        if (r.length < 24 || r.slice(20, 22) != '00') // StatusFlag1 != STA1_NORMAL
+        if (r.length < 26 || r.slice(20, 22) != '00') // StatusFlag1 != STA1_NORMAL
             return null;
         else
-            return r.slice(24);
+            return r.slice(26);
     }
 }
 
