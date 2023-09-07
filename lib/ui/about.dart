f704be256c6fc661a3c6fd09e8854cafda3ced48
Harry Chen <harry-chen@outlook.com>
2023-09-07 17:18:20 +0800

    Upgrade build tools, finally fix compilation after two days...
    Signed-off-by: Harry Chen <harry-chen@outlook.com>

--- lib/ui/about.dart
+++ lib/ui/about.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import 'widgets.dart';
